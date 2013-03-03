256fd56f961970d8cd4062f066b2cd6f2479ff1b
kamagatos <kamagatos@gmail.com>
2013-03-03 07:11:45 -0500

    add clean password to userchangeform

--- registration_withemail/forms.py
+++ registration_withemail/forms.py
@@ -68,6 +68,12 @@ class UserChangeForm(forms.ModelForm):
     class Meta:
         model = EldonUser
 
+    def clean_password(self):
+        # Regardless of what the user provides, return the initial value.
+        # This is done here, rather than on the field, because the
+        # field does not have access to the initial value
+        return self.initial["password"]        
+
 
 class EldonUserAuthenticationForm(AuthenticationForm):
     """
