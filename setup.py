d721c6b4c63257aedca389ae3d7683a8343299a2
kamagatos <kamagatos@gmail.com>
2013-03-03 07:27:08 -0500

    update version 1.1.3

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ README = open('README.md').read()
 
 setup(
 		name='django-registration-withemail',
-		version='0.1.2',
+		version='0.1.3',
 		description='This is a simple user-registration application for Django, designed to make allowing user signups as painless as possible.',
 		packages=find_packages(),
 		include_package_data=True,
